f09aa4e47b7366691366467efc20e15b45c1c811
Robin Richard Arulanantham <arulr1@student.op.ac.nz>
2018-10-02 15:48:49 +1300

    server codes

--- server/Reconnaissance/Reconnaissance/urls.py
+++ server/Reconnaissance/Reconnaissance/urls.py
@@ -13,17 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
-from django.urls import include, path
-urlpatterns = [
-     path('', include('Campus.urls')),
-    url(r'^admin/', admin.site.urls),
-]
+# from django.conf.urls import url
 # from django.contrib import admin
 # from django.urls import include, path
-
 # urlpatterns = [
 #     path('', include('Campus.urls')),
-#     path('admin/', admin.site.urls),
+#     url(r'^new/',  include('Campus.urls')),
+#     url(r'^admin/', admin.site.urls),
 # ]
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    # path('', include('Campus.urls')),
+    path('admin/', admin.site.urls),
+]
