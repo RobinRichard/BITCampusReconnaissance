4df8188e4b1bd5eef8cb157bc792f62f1706103a
Robin Richard Arulanantham <arulr1@student.op.ac.nz>
2018-11-22 16:23:08 +1300

    reset

--- Reconnaissance/Campus/static/Campus/js/reconnaissance.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#wrapper').on('keyup','.clrValidation', function(){
-        $(this).parent('div').find('p').text(' ')
-    });
-    $('#wrapper').on('change','.clrValidation', function(){
-        $(this).parent('div').find('p').text(' ')
-    })
-});
